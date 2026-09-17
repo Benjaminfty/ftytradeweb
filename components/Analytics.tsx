a041e03bb7771c4bed5e2a95aa60549acce1c9f6
Ayman Elotfi <lotfiaymanee@gmail.com>
2026-09-17 18:52:42 +0200

    meta pixel

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -3,6 +3,7 @@
 import Script from "next/script";
 
 const GA_ID = "G-WFV5C336D3";
+const META_PIXEL_ID = "1951898582686341";
 
 declare global {
   interface Window {
@@ -10,21 +11,24 @@ declare global {
     gtag?: (...args: any[]) => void;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     dataLayer?: any[];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    fbq?: (...args: any[]) => void;
   }
 }
 
 export function trackLead(lang: string) {
   try {
-    window.gtag?.("event", "generate_lead", {
-      method: "waitlist_form",
-      language: lang,
-    });
+    window.gtag?.("event", "generate_lead", { method: "waitlist_form", language: lang });
+  } catch {}
+  try {
+    window.fbq?.("track", "Lead", { content_name: "waitlist", language: lang });
   } catch {}
 }
 
 export default function Analytics() {
   return (
     <>
+      {/* Google Analytics 4 */}
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
       <Script id="ga4-init" strategy="afterInteractive">
         {`
@@ -34,6 +38,32 @@ export default function Analytics() {
           gtag('config', '${GA_ID}');
         `}
       </Script>
+
+      {/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt=""
+          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
     </>
   );
 }
